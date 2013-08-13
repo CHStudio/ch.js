2b48b157138375d810e8bac4bf29fbe9eb3d471c
Stéphane HULARD <s.hulard@gmail.com>
2013-08-13 18:38:12 +0200

    Create some native object extensions and tools without modifying the default implementation
    Signed-off-by: Stéphane HULARD <s.hulard@gmail.com>

---
+++ src/native/Array.js
@@ -0,0 +1,90 @@
+// CH JavaScript Framework
+
+// This file is part of the CH JavaScript Framework.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
+/**
+ * Array manipulation object: Extend JavaScript native Array definition
+ * @author Stephane HULARD <s.hulard@chstudio.fr>
+ * @author Simon FREMAUX <simon.fremaux@gmail.com>
+ * @copy CHStudio <www.chstudio.fr> 2013
+ * @package native
+ */
+(function() {
+	'use strict';
+
+	//Define a CH namespace inside the native prototype to avoid method collision
+	ch.define('ch.native.Array', function( module_ ) {;
+
+		module_ = function() {};
+
+		/**
+		 * Clone current object and return the new instance
+		 * @param {Array} aArray Array to be processed
+		 * @return Array
+		 */
+		module_.clone = function( aArray ) {
+			var oClone = new Array();
+			for ( var i = 0; i <  aArray.length; i++)
+				oClone.push(aArray[i]);
+
+			return oClone;
+		};
+
+		/**
+		 * Max value accessor inside an array
+		 * usable only if array contain numbers
+		 * @param {Array} aArray Array to be processed
+		 * @param {Mixed} mEntity Item to check existence
+		 * @return Number
+		 */
+		module_.in = function( aArray, mEntity ) {
+			return aArray.indexOf(mEntity) !== -1;
+		};
+
+		/**
+		 * Max value accessor inside an array
+		 * usable only if array contain numbers
+		 * @param {Array} aArray Array to be processed
+		 * @return Number
+		 */
+		module_.max = function( aArray ) {
+			var mMax;
+			for ( var i = 0; i < aArray.length; i++)
+				if( parseInt(aArray[i], 10) > mMax || !mMax)
+					mMax = parseInt(aArray[i], 10);
+
+			return mMax;
+		};
+
+		/**
+		 * Min value accessor inside an array
+		 * usable only if array contain numbers
+		 * @param {Array} aArray Array to be processed
+		 * @return Number
+		 */
+		module_.min = function( aArray ) {
+			var mMin;
+			for ( var i = 0; i < aArray.length; i++)
+				if( parseInt(aArray[i], 10) > mMin || !mMin)
+					mMin = parseInt(aArray[i], 10);
+
+			return mMin;
+		};
+
+		/**
+		 * Random sort an Array
+		 * @param {Array} aArray Array to be processed
+		 */
+		module_.shuffle = function(aArray) {
+			var aResult = new Array;
+			while( aArray.length )
+				aResult.push(aArray.splice(Math.random() * aArray.length, 1));
+			while( aResult.length )
+				aArray.push(aResult.pop());
+		};
+
+		return module_;
+	});
+}).call(this);
